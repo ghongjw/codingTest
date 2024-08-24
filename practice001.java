eb0a971df50e3701de440b7939c7c31d0ff8139f
ghongjw <ghongjw@gmail.com>
2024-08-24 23:19:21 +0900

    practice003

--- practice001.java
+++ practice001.java
@@ -1,6 +1,8 @@
 import java.util.Scanner;
-
 public class practice001 {
+    //N개의 숫자가 공백없이 써있다. 이숫자를 모두 합해 출력하는 프로그램을 만들어라.
+    //1번째 줄에 숫자의 개수 N(1<=N<=100)2번째 줄에 숫자 N개가 공백없이 주어진다.
+    //ex 입력 >5 >54321 출력 > 15 
     public static void main(String[] args){
 
         Scanner scanner = new Scanner(System.in);
--- practice003.java
+++ practice003.java
@@ -1,7 +1,5 @@
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
-import java.io.BufferedReader;
+import java.io.*;
+import java.util.*;
 
 public class practice003 {
     //0.5초안에 나와야 한다.
@@ -9,10 +7,32 @@ public class practice003 {
 //3번째 줄부터는 M개의 줄에 합을 구해야하는 구간i와j가 주어진다
 //총 M개의 줄에 입력으로 주어진 i번째 수에서j번째 수 까지의 합을 출력한다.
 //이렇게 숫자가 많을때는 합배열을 이용하는것이 좋다
-    public static void main(String[] args) throws IOException {
-        //입력 받는 숫자가 많을 때에는 버퍼드리더가 유리하다.
-        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+public static void main(String[] args) throws IOException{
+    BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+    String line1 = bufferedReader.readLine();
+    StringTokenizer stringTokenizer = new StringTokenizer(line1," ");
+    
+    int suNo = Integer.parseInt(stringTokenizer.nextToken());
+    int quizNo = Integer.parseInt(stringTokenizer.nextToken());
+    long S[] = new long[suNo+1];
+
+    String line2 = bufferedReader.readLine();
+    stringTokenizer = new StringTokenizer(line2," ");
+    
+    for(int i = 1; i<=suNo; i++){
+        S[i] = S[i-1] + Integer.parseInt(stringTokenizer.nextToken());
     }
+    for(int q=0;q<quizNo;q++){
+        String line3 = bufferedReader.readLine();
+        StringTokenizer stringTokenizer3 = new StringTokenizer(line3," ");
+        
+    int i = Integer.parseInt(stringTokenizer3.nextToken());
+    int j = Integer.parseInt(stringTokenizer3.nextToken());
+        
+    System.out.println(S[j]-S[i-1]);
+        
+    }
+    
+}
 
 }
